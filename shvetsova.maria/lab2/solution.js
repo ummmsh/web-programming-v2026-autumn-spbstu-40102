3b87c22c1c7329786f7ab4e8e0286f1dd9b1ad01
Maria Shvetsova <airaaaam.msh@gmail.com>
2026-09-13 19:01:10 +0300

    [TASK-2] variant_28 shvetsova.maria

---
+++ shvetsova.maria/lab2/solution.js
@@ -0,0 +1,27 @@
+export function calculateExpression(expr) {
+  const tokens = expr.match(/\d+\.?\d*|[+\-*/]/g);
+  if (!tokens) throw new Error('invalid expression');
+
+  let firstStage = [parseFloat(tokens[0])];
+  for (let i = 1; i < tokens.length - 1; i += 2) {
+    const op = tokens[i];
+    const num = tokens[i + 1];
+    if (op === '/' || op === '*') {
+      const last = parseFloat(firstStage.pop());
+      const result = op === '/' ? last / num : last * num;
+      firstStage.push(result);
+    } else {
+      firstStage.push(op);
+      firstStage.push(num);
+    }
+  }
+
+  let result = firstStage[0];
+  for (let i = 1; i < firstStage.length - 1; i += 2) {
+    const op = firstStage[i];
+    const num = firstStage[i + 1];
+    result = op === '+' ? result + num : result - num;
+  }
+
+  return result;
+}
